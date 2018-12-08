d7eec01282da590f6500693976219840d1b4395f
moblisIO <45700017+moblisIO@users.noreply.github.com>
2018-12-08 12:14:48 -0500

    Update Cutout.java

--- src/android/Cutout.java
+++ src/android/Cutout.java
@@ -1,22 +1,34 @@
 package io.moblis.cordova.plugins;
 
-import java.util.Locale;
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
-import org.apache.cordova.PluginResult;
+import org.apache.cordova.*;
 import org.json.JSONArray;
-
-import android.content.Context;
+import org.json.JSONException;
 
 public class Cutout extends CordovaPlugin {
 
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+    @Override
+    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+
         if (action.equals("get")) {
-            String result = Locale.getDefault().getCountry();
-            callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, result));
+
+            //String name = data.getString(0);
+            String message = "Hello";
+            callbackContext.success(message);
             return true;
+
+        } else {            
+            return false;
         }
-        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR));
-        return false;
-    }
+    }   
+    
+    
+    //public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+    //    if (action.equals("get")) {
+    //        String result = Locale.getDefault().getCountry();
+    //        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, result));
+    //        return true;
+    //    }
+    //    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR));
+    //    return false;
+    //}
 }
