1053e3eb2fbdd6444b35fb46ecf864134f0c9f8a
moblisIO <45700017+moblisIO@users.noreply.github.com>
2018-12-08 12:34:41 -0500

    Update cutout.js

--- www/cutout.js
+++ www/cutout.js
@@ -3,22 +3,3 @@ module.exports = {
         cordova.exec(successCallback, errorCallback, "Cutout", "get", []);
     }
 };
-
-/*global cordova, module
-
-var Cutout = function() {};
-
-Cutout.prototype.get = function(success, fail) {
-  cordova.exec(success, fail, 'Cutout', 'get', []);
-};
-
-if (!window.plugins) {
-  window.plugins = {};
-}
-if (!window.plugins.cutout) {
-  window.plugins.cutout = new Cutout();
-}
-
-if (module.exports) {
-  module.exports = Cutout;
-}*/
