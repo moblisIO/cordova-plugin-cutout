c7699cdc7a325e58c06a261f30b0ea580b5ccedb
moblisIO <45700017+moblisIO@users.noreply.github.com>
2018-12-08 13:32:03 -0500

    Update Cutout.java

--- src/android/Cutout.java
+++ src/android/Cutout.java
@@ -1,23 +1,37 @@
-package io.moblis.cordova.plugins;
+package io.moblis.plugin;
 
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+
+import android.app.Activity;
 
 public class Cutout extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals("get")) {
+        if (action.equals("hasCutout")) {            
 
-            //String name = data.getString(0);
-            String message = "Hello";
-            callbackContext.success(message);
+            final Activity activity = this.cordova.getActivity();
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+                android.view.DisplayCutout displayCutout = activity.getWindow().getDecorView().getRootWindowInsets().getDisplayCutout();
+                if (displayCutout != null) {
+                    callbackContext.success(1);
+                    return true;
+                } else {
+                    callbackContext.success(0);
+                    return true;
+                }
+            }
+            callbackContext.success(0);
+
             return true;
 
-        } else {            
+        } else {
+            
             return false;
+
         }
     }
 }
